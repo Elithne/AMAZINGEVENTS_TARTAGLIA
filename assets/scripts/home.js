7c5af0730cee7c8e02a67c4f8f0827cd1a1e4e9c
Giannina <gftartaglia@gmail.com>
2023-08-30 17:06:02 -0300

    fixes responsiveness

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,2 +1,5 @@
 let cardsContainer = document.getElementById('cardsContainer');
 createCards(data.events, "cardsContainer");
+
+let checkboxContainer = document.getElementById('checkboxContainer');
+createCategories(data.events, "checkboxContainer");
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,13 @@
-
-
 function createCards(arrayItems, containerId) {
     for(let item of arrayItems){
         let card = `
             <div class="col-12 col-sm-6 col-lg-4 col-xl-3 my-1">
-                <div class="card p-2 m-2 shadow-sm" style="width: 18rem;">
+                <div class="card p-2 m-2 shadow-sm";">
                     <img src="${item.image}" class="card-img-top" alt="${item.name}">
                     <div class="card-body text-center">
                         <h5 class="card-title">${item.name}</h5>
                         <p class="card-text">${item.description}</p>
-                        <div class="d-flex justify-content-between">
+                        <div class="d-flex justify-content-md-between flex-column">
                             <p class="card-text p-2">Price: $${item.price}</p>                           
                             <div>
                                 <a href="details.html" class="btn btn-danger">Details</a>
@@ -19,6 +17,18 @@ function createCards(arrayItems, containerId) {
                 </div>
             </div>`; 
         cardsContainer.innerHTML += card; 
+    }
+};
+
+function createCategories(arrayItems, containerId) {
+    for(let item of arrayItems){
+        
+        let checkbox = `
+        <label class="form-check-label p-3">
+        <input class="form-check-input" type="checkbox" name="" value="">
+        <span class="form-option">${item.category}</span>
+        </label> `; 
+        checkboxContainer.innerHTML += checkbox; 
     }
 };
 
